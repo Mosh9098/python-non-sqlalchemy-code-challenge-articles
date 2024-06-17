9313b4ee7981e893e2654ca26cc69a73a9644d3a
mosh9098 <mosh9098@gmail.com>
2024-06-18 00:46:07 +0300

    complete

--- lib/author.py
+++
@@ -1,9 +0,0 @@
-class Author:
-    def __init__(self, name):
-        if isinstance(name, str) and len(name) > 0:
-            self.__name = name
-        else:
-            raise ValueError("Name must be a non-empty string.")
-
-    def name(self):
-        return self.__name
