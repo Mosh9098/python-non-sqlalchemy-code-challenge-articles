7d296e1f468b6fb4af99e949b412e429685f7fb6
mosh9098 <mosh9098@gmail.com>
2024-06-18 01:22:31 +0300

    complete

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,3 +1,49 @@
+class Article:
+    all = []
+
+    def __init__(self, author, magazine, title):
+        if not isinstance(author, Author):
+            raise ValueError("Author must be of type Author")
+        if not isinstance(magazine, Magazine):
+            raise ValueError("Magazine must be of type Magazine")
+        if not isinstance(title, str) or not (5 <= len(title) <= 50):
+            raise ValueError("Title must be a string between 5 and 50 characters")
+        self._author = author
+        self._magazine = magazine
+        self._title = title
+        author._articles.append(self)
+        magazine._articles.append(self)
+        Article.all.append(self)
+
+    @property
+    def title(self):
+        return self._title
+
+    @property
+    def author(self):
+        return self._author
+
+    @author.setter
+    def author(self, value):
+        if not isinstance(value, Author):
+            raise ValueError("Author must be of type Author")
+        self._author._articles.remove(self)
+        self._author = value
+        self._author._articles.append(self)
+
+    @property
+    def magazine(self):
+        return self._magazine
+
+    @magazine.setter
+    def magazine(self, value):
+        if not isinstance(value, Magazine):
+            raise ValueError("Magazine must be of type Magazine")
+        self._magazine._articles.remove(self)
+        self._magazine = value
+        self._magazine._articles.append(self)
+
+
 class Author:
     def __init__(self, name):
         if not isinstance(name, str) or len(name) == 0:
@@ -9,15 +55,15 @@ class Author:
     def name(self):
         return self._name
 
+    def add_article(self, magazine, title):
+        article = Article(self, magazine, title)
+        return article
+
     def articles(self):
         return self._articles
 
     def magazines(self):
         return list(set(article.magazine for article in self._articles))
-
-    def add_article(self, magazine, title):
-        article = Article(self, magazine, title)
-        return article
 
     def topic_areas(self):
         categories = list(set(article.magazine.category for article in self._articles))
@@ -57,6 +103,10 @@ class Magazine:
             raise ValueError("Category must be a non-empty string")
         self._category = value
 
+    def add_article(self, author, title):
+        article = Article(author, self, title)
+        return article
+
     def articles(self):
         return self._articles
 
@@ -77,45 +127,3 @@ class Magazine:
         if not cls.all:
             return None
         return max(cls.all, key=lambda mag: len(mag.articles()), default=None)
-
-
-class Article:
-    all = []
-
-    def __init__(self, author, magazine, title):
-        if not isinstance(author, Author):
-            raise ValueError("Author must be of type Author")
-        if not isinstance(magazine, Magazine):
-            raise ValueError("Magazine must be of type Magazine")
-        if not isinstance(title, str) or not (5 <= len(title) <= 50):
-            raise ValueError("Title must be a string between 5 and 50 characters")
-        self._author = author
-        self._magazine = magazine
-        self._title = title
-        author._articles.append(self)
-        magazine._articles.append(self)
-        Article.all.append(self)
-
-    @property
-    def title(self):
-        return self._title
-
-    @property
-    def author(self):
-        return self._author
-
-    @author.setter
-    def author(self, value):
-        if not isinstance(value, Author):
-            raise ValueError("Author must be of type Author")
-        self._author = value
-
-    @property
-    def magazine(self):
-        return self._magazine
-
-    @magazine.setter
-    def magazine(self, value):
-        if not isinstance(value, Magazine):
-            raise ValueError("Magazine must be of type Magazine")
-        self._magazine = value
